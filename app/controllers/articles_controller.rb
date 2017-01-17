66e1c5d5a706dd43d0de60d3115e8e6f38280f35
Jeff Lynch <lynchjt.90@gmail.com>
2017-01-17 23:19:01 +0000

    Added article actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,43 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
   def new
     @article = Article.new
   end
   
-  def show
-    
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.new(article_params)
+
+    if @article.update(article_params)
+      flash[:notice] = "Article Was Updated"
+      redirect_to article_path(@article)
+
+    else 
+      render 'edit'
+    end
+
   end
 
+  def show
+    @article = Article.find(params[:id])  
+  end
+  
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article Was Created"
+      redirect_to article_path(@article)
+    else 
+      render 'new'
+    end
+
   end
   
   private
